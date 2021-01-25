8e2c1ca48ddf8e94f8c39b803bf9066b43e5bcca
Simon Broggi <simon.broggi@insert-coin.ch>
2021-01-25 18:15:43 +0100

    axis aligned clipping box

---
+++ Assets/Test/PointCliper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Pcx;
+
+[ExecuteInEditMode]
+public class PointCliper : MonoBehaviour
+{
+    [SerializeField] PointCloudData _sourceData = null;
+    [SerializeField] ComputeShader _computeShader = null;
+
+    [SerializeField] Bounds clipBounds;
+
+    ComputeBuffer _pointBuffer;
+    void Update()
+    {
+        if (_sourceData == null) return;
+
+        var sourceBuffer = _sourceData.computeBuffer;
+
+        if (_pointBuffer == null || _pointBuffer.count != sourceBuffer.count)
+        {
+            if (_pointBuffer != null) _pointBuffer.Release();
+            _pointBuffer = new ComputeBuffer(sourceBuffer.count, PointCloudData.elementSize, ComputeBufferType.Append);
+        }
+        
+        _pointBuffer.SetCounterValue(0);
+        
+        var kernel = _computeShader.FindKernel("Main");
+        _computeShader.SetVector ("clipMin", clipBounds.min);
+        _computeShader.SetVector ("clipMax", clipBounds.max);
+        _computeShader.SetBuffer(kernel, "SourceBuffer", sourceBuffer);
+        _computeShader.SetBuffer(kernel, "OutputBuffer", _pointBuffer);
+        _computeShader.Dispatch(kernel, sourceBuffer.count / 128, 1, 1);
+
+
+        GetComponent<PointCloudRenderer>().sourceBuffer = _pointBuffer;
+    }
+}
