f0b6d73d7cb1770b535964d88a761a7cbfeee0ce
Simon Broggi <simon.broggi@insert-coin.ch>
2021-01-26 22:03:21 +0100

    dizimate points

--- Assets/Test/PointClipperMesh.cs
+++ Assets/Test/PointClipperMesh.cs
@@ -14,16 +14,24 @@ public class PointClipperMesh : MonoBehaviour
 
     void OnEnable()
     {
-        _mesh = InitializeMesh();
+        _mesh = GetComponent<MeshFilter>().mesh;
+        _indices = new NativeArray<int>(_mesh.vertexCount / decimator, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+
+        UpdateMesh();
     }
     void OnDisable()
     {
-        _vertices.Dispose();
+        // _vertices.Dispose();
         _indices.Dispose();
     }
     public int decimator = 1;
     void Update()
     {
+        // dont create a new array here. probably need a native list
+        // _indices.Dispose();
+        _indices = new NativeArray<int>(_mesh.vertexCount / decimator, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+        UpdateMesh();
+
         // _indices = new NativeArray<int>(_mesh.vertexCount / decimator, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
         // for (int i = 0; i < _mesh.vertexCount / decimator; i++)
         // {
@@ -48,37 +56,14 @@ public class PointClipperMesh : MonoBehaviour
 
 	// 	}
     // }
-    Mesh InitializeMesh()
+    Mesh UpdateMesh()
     {
-        Debug.Log("init mesh");
-        Mesh newMesh = new Mesh();
-        Mesh originalMesh = GetComponent<MeshFilter>().mesh;
-        Debug.Log("copy stuff");
-        // coppy vertices. Should it point to original mesh data?
-        _vertices = new NativeArray<Vector3>(originalMesh.vertexCount, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-        for (int i = 0; i < originalMesh.vertexCount; i++)
-        {
-            _vertices[i] = originalMesh.vertices[i];
-        }
-        newMesh.SetVertices(_vertices);
-        Debug.Log("verts set");
-        
-        _indices = new NativeArray<int>(originalMesh.vertexCount / decimator, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-        for (int i = 0; i < originalMesh.vertexCount / decimator; i++)
+        for (int i = 0; i < _mesh.vertexCount / decimator; i++)
         {
             _indices[i] = i * decimator;
         }
-        newMesh.SetIndices(_indices, MeshTopology.Points, 0);
-        Debug.Log("indices set");
+        _mesh.SetIndices(_indices, MeshTopology.Points, 0);
 
-        System.Collections.Generic.List<Color32> colors = new System.Collections.Generic.List<Color32>(originalMesh.vertexCount);
-        originalMesh.GetColors(colors);
-        newMesh.SetColors(colors);
-
-        newMesh.UploadMeshData(false);
-        
-        GetComponent<MeshFilter>().mesh = newMesh;
-
-        return newMesh;
+        return _mesh;
     }
 }
